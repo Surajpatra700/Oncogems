3443e2df029c2211e7159125756807e16b95d887
Suraj Patra <surajpatra2908@gmail.com>
2023-08-08 20:20:44 +0530

    Achievement screen completed

--- lib/screens/achievement.dart
+++ lib/screens/achievement.dart
@@ -1,3 +1,4 @@
+import 'package:achiever/screens/plants_Screens/plantScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -13,7 +14,21 @@ class _AchievementScreenState extends State<AchievementScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Achievement"),),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color(0xff61B688).withOpacity(0.7),
+            Color(0xff61B688).withOpacity(0.5),
+            Color(0xff61B688).withOpacity(0.4),
+          ],
+          begin: FractionalOffset(0.0, 0.0),
+          end: FractionalOffset(0.0, 1.0),
+          stops: [0.0, 0.5, 1.0],
+          tileMode: TileMode.clamp
+          ),
+        ),
+        child: PlantScreen(),
+      ),
     );
   }
 }
