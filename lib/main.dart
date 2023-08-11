c1b824762d3b9fc15a41694b5551f79a2f37ef0f
Suraj Patra <surajpatra2908@gmail.com>
2023-08-11 23:15:36 +0530

    onboarding Screen homepage completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:achiever/screens/authentications/signupPage.dart';
+import 'package:achiever/screens/onboarding%20Screens/onboarding_home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,6 +7,7 @@ import 'package:get/get.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Get.put(WelcomeController());
   runApp(const MyApp());
 }
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.green,
           ),
-          home: SignUp(),
+          home: UnboardingPage(),
         );
       },
     );
---
+++ lib/screens/onboarding Screens/onboardingScreen1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreenOne extends StatefulWidget {
+  const OnboardingScreenOne({super.key});
+
+  @override
+  State<OnboardingScreenOne> createState() => _OnboardingScreenOneState();
+}
+
+class _OnboardingScreenOneState extends State<OnboardingScreenOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Onboarding Screen 1"),
+      ),
+    );
+  }
+}
